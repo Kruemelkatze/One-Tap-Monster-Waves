fd1a56e73f3ab3cf3e79614b7671a8c567cd6851
Fabian <schoberf@gmx.net>
2017-01-20 20:11:04 +0100

    Basic GameLogics

---
+++ OneTapMonsterWaves/Assets/_Scripts/Common/EventHub.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventHub : MonoBehaviour {
+
+	#region Event delegates
+	public delegate void VoidEvent();
+	public delegate void IntegerParamEvent (int value);
+	public delegate void BoolParamEvent (bool value);
+	public delegate void GameObjectParamEvent(GameObject obj);
+	public delegate void GameObjectIntegerParamEvent(GameObject enemy, int value);
+	public delegate void GameObjectBoolParamEvent(GameObject enemy, bool value);
+	public delegate void MovementParamEvent(Vector3 position, Vector3 location, bool lerp = false, bool rotationReset = false);
+
+	#endregion
+
+
+	#region Events
+
+	public event VoidEvent TestEvent;
+
+	public event GameObjectParamEvent TestGOEvent;
+
+	#endregion
+
+	#region Triggers
+	public void TriggerTestEvent() {
+		if(TestEvent != null)
+			TestEvent ();
+	}
+
+	public void TriggerUpdateObjectEvent(GameObject obj)
+	{
+		if (TestGOEvent != null)
+			TestGOEvent (obj);
+	}
+
+	//You get the idea on how this is done...
+	//Generic .Invoke(...) cannot be done, as the event fields cannot be accessed from outside this class :(
+	#endregion
+}
---
+++ OneTapMonsterWaves/Assets/_Scripts/Common/SoundManager.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+	public AudioSource[] efxSource;
+	public AudioSource loopMusic;
+
+	public AudioClip OnAnySound;
+
+	public AudioClip defaultTheme;
+	public float defaultThemeVolume = 1;
+
+	public AudioClip currentTheme;
+	public float currentThemeVolume = 1;
+
+	public AudioClip StandardTheme;
+
+	private int nextSourceToUse = 0;
+	public float lowPitchRange = .95f;
+	public float highPitchRange = 1.05f;
+
+	void Awake(){
+		if (efxSource.Length == 0) {
+			efxSource = new AudioSource[10];
+
+			for (int i = 0; i < efxSource.Length; i++) {
+				efxSource[i] = new AudioSource();
+			}
+		}
+
+		currentTheme = defaultTheme;
+		PlayCurrentTheme ();
+
+		//RegisterEvents
+	}
+
+	void OnDestroy() {
+		//Unregister Events
+	}
+
+	public void PlaySingle(AudioClip clip, float volume = 1)
+	{
+		int index = nextSourceToUse;
+		nextSourceToUse = (nextSourceToUse + 1) % efxSource.Length;
+		efxSource [index].clip = clip;
+		efxSource [index].volume = volume;
+		efxSource [index].Play ();
+	}
+
+	public void RandomizeSfx (float volume, params AudioClip[] clips)
+	{
+		if (volume < 0) {
+			volume = 0;
+		} else if (volume > 1) {
+			volume = 1;
+		}
+		int randomIndex = Random.Range(0, clips.Length);
+
+		float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+
+		int index = nextSourceToUse;
+		nextSourceToUse = (nextSourceToUse + 1) % efxSource.Length;
+
+		efxSource[index].pitch = randomPitch;
+
+		efxSource[index].clip = clips[randomIndex];
+		efxSource [index].volume = volume;
+		efxSource[index].Play();
+	}
+
+	//Music controller
+	public void SetCurrentTheme(AudioClip clip, float volume = 0.3f) {
+		bool sameTheme = clip.name.Equals (currentTheme.name);
+
+		currentThemeVolume = volume;
+
+		if (!sameTheme) {
+			currentTheme = clip;
+			PlayCurrentTheme ();
+		}
+	}
+
+	public void SetDefaultThemeAsCurrentTheme() {
+		SetCurrentTheme (defaultTheme, defaultThemeVolume);
+	}
+
+	public void PlayCurrentTheme() {
+		PlayMusic (currentTheme, currentThemeVolume);
+	}
+
+	public void PlayMusic(AudioClip clip, float volume = 0.3f) {
+		loopMusic.Stop ();
+		loopMusic.loop = true;
+		loopMusic.clip = clip;
+		loopMusic.volume = volume;
+		loopMusic.Play ();
+	}
+}
